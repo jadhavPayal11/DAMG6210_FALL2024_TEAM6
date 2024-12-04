9bc3d1a9935e6f508f19d38a443afc496524bf90
jadhavPayal11 <jadhav.pay@northeastern.edu>
2024-12-03 20:14:10 -0500

    Stored Procedure tp Process Payment and testcases

---
+++ Packages/PaymentManagement/SPTestCase.sql
@@ -0,0 +1,16 @@
+BEGIN
+    ProcessPayment(1); -- Replace 101 with an actual Payment ID
+END;
+/
+
+BEGIN
+    ProcessPayment(3); -- Replace 101 with an actual Payment ID
+END;
+/
+
+BEGIN
+    ProcessPayment(5); -- Replace 101 with an actual Payment ID
+END;
+/
+
+
