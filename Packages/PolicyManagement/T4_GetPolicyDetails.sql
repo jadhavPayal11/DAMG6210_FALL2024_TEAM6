2712a29d52f5fce513c21d3a6792fb5d16e36517
MansiGondil <gondil.m@northeastern.edu>
2024-12-04 22:12:51 -0500

    PKG: policymanagement changes

--- Packages/PolicyManagement/T4_GetPolicyDetails.sql
+++
@@ -1,5 +0,0 @@
-SELECT policy_management_package.GetPolicyDetailsWrapper(3) AS policy_details FROM dual;
--- Expected Output: Full details of the policy in a formatted string
-
-SELECT policy_management_package.GetPolicyDetailsWrapper(999) AS policy_details FROM dual;
--- Expected Output: Error - "Policy does not exist."
