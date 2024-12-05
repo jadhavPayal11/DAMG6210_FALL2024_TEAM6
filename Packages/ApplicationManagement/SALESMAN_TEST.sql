92f5517fc943931a31f8b1cf2fdef512f7cd9018
Sushma-Mangalampati <mangalampati.s@northeastern.edu>
2024-12-04 20:13:23 -0500

    added test cases for get_application_status function

--- Packages/ApplicationManagement/SALESMAN_TEST.sql
+++ Packages/ApplicationManagement/SALESMAN_TEST.sql
@@ -40,11 +40,22 @@ BEGIN
 END;
 /
 
-/*DECLARE
-v_application_status ICPS_CORE.INSURANCE_APPLICATION.APPLICATION_ID%TYPE;
+-- Get status of a valid application
+DECLARE
+v_application_status ICPS_CORE.INSURANCE_APPLICATION.STATUS%TYPE;
 BEGIN
     v_application_status := ICPS_CORE.GET_APPLICATION_STATUS_WRAPPER_FUNC(
                                 p_application_id => 1);
     dbms_output.put_line('Application status is: ' || v_application_status);
 END;
-/*/
+/
+
+-- Get status of an invalid application
+DECLARE
+v_application_status ICPS_CORE.INSURANCE_APPLICATION.STATUS%TYPE;
+BEGIN
+    v_application_status := ICPS_CORE.GET_APPLICATION_STATUS_WRAPPER_FUNC(
+                                p_application_id => 8);
+    dbms_output.put_line('Application status is: ' || v_application_status);
+END;
+/
