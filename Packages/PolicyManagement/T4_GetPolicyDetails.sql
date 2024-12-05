330b692656393802e57754bd8e39010fd68e7904
MansiGondil <gondil.m@northeastern.edu>
2024-12-04 19:02:28 -0500

    Added wrappers for procedures and fucntions then updated the testcase files accordingly

--- Packages/PolicyManagement/T4_GetPolicyDetails.sql
+++ Packages/PolicyManagement/T4_GetPolicyDetails.sql
@@ -1,5 +1,5 @@
-SELECT policy_management_package.GetPolicyDetails(3) AS policy_details FROM dual;
+SELECT policy_management_package.GetPolicyDetailsWrapper(3) AS policy_details FROM dual;
 -- Expected Output: Full details of the policy in a formatted string
 
-SELECT policy_management_package.GetPolicyDetails(999) AS policy_details FROM dual;
+SELECT policy_management_package.GetPolicyDetailsWrapper(999) AS policy_details FROM dual;
 -- Expected Output: Error - "Policy does not exist."
